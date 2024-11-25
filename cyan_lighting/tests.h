e0be73d168034d9e4249ceda8a8a6f925bedbc12
Max <bitmaxalgo@gmail.com>
2024-11-25 16:47:23 +0000

    Created cyan_lighting

---
+++ cyan_lighting/tests.h
@@ -0,0 +1,7 @@
+
+#ifndef TESTS_H
+#define TESTS_H
+void processEvents(GLFWwindow* window);
+void framebuffercb(GLFWwindow* window, int width, int height);
+void updateUniforms(GLint wloc, GLint hloc);
+#endif
